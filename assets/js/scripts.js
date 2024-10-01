d52bb426615f88978791404ce83204f58381d048
Oleksandr Dorobalo <al.dorobalo@gmail.com>
2024-10-01 07:31:49 +0200

    header

---
+++ assets/js/scripts.js
@@ -0,0 +1,51 @@
+// Burger Menu Open //
+document.addEventListener("DOMContentLoaded", function () {
+  // Выбираем бургер-кнопку и навигацию
+  let burgerButton = document.getElementById("burgerButton");
+  let navigation = document.querySelector(".navigation");
+  let links = document.querySelectorAll(".navigation__link");
+
+  // Если бургер-кнопка существует, добавляем обработчик события
+  if (burgerButton) {
+    burgerButton.addEventListener("click", function (e) {
+      e.stopPropagation(); // Остановка всплытия события
+      burgerButton.classList.toggle("burger--active"); // Переключаем класс активности бургер-кнопки
+      navigation.classList.toggle("navigation--active"); // Переключаем класс активности навигации
+    });
+  }
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      burgerButton.classList.remove("burger--active");
+      navigation.classList.remove("navigation--active");
+      console.log("link", link);
+    });
+  });
+});
+//
+
+// Fixed header
+$(document).ready(function () {
+  var header = $(".header"),
+    main = $(".main"),
+    headerH = header.innerHeight(),
+    scrollOffset = $(window).scrollTop();
+
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function () {
+    scrollOffset = $(this).scrollTop();
+    checkScroll(scrollOffset);
+  });
+
+  function checkScroll(scrollOffset) {
+    if (scrollOffset >= headerH) {
+      header.addClass("fixed");
+      main.css("padding-top", headerH); // Добавляем верхний отступ
+    } else {
+      header.removeClass("fixed");
+      main.css("padding-top", 0); // Убираем верхний отступ
+    }
+  }
+
+});
