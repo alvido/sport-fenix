8fc3540968d183393d8a700f5efac7dff5496795
Oleksandr Dorobalo <al.dorobalo@gmail.com>
2024-10-01 14:16:43 +0200

    hero

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -49,3 +49,119 @@ $(document).ready(function () {
   }
 
 });
+//
+
+//language select
+$(document).ready(function () {
+  function formatState(state) {
+    if (!state.id) {
+      return state.text;
+    }
+    // Добавляем флаг в выпадающий список
+    var baseUrl = $(state.element).data('image');
+    var $state = $(
+      '<span><img src="' + baseUrl + '" class="img-flag" /> ' + state.text + '</span>'
+    );
+    return $state;
+  }
+
+  // Инициализируем select2 с кастомным отображением
+  $('#language-select').select2({
+    templateResult: formatState,
+    templateSelection: formatState,
+    minimumResultsForSearch: Infinity // скрываем строку поиска, если не нужна
+  });
+});
+//
+
+//currency select
+$(document).ready(function () {
+  function formatCurrency(state) {
+    if (!state.id) {
+      return state.text;
+    }
+    var symbol = $(state.element).data('symbol'); // Получаем символ из data-атрибута
+    var $state = $(
+      '<span><span class="currency-symbol">' + symbol + '</span>' + state.text + '</span>'
+    );
+    return $state;
+  }
+
+  // Инициализируем select2 с кастомным отображением символов валют
+  $('#currency-select').select2({
+    templateResult: formatCurrency,    // Форматирование выпадающего списка
+    templateSelection: formatCurrency, // Форматирование выбранного значения
+    minimumResultsForSearch: Infinity, // Отключаем поиск
+  });
+});
+//
+
+//swiper
+document.addEventListener("DOMContentLoaded", function () {
+  // Функция для инициализации слайдера
+  function initSwiper(selector, paginationClass, nextButtonClass, prevButtonClass) {
+    if (document.querySelector(selector)) {
+      new Swiper(selector, {
+        observer: true,
+        observeParents: true,
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: paginationClass,
+          clickable: true,
+        },
+        navigation: {
+          nextEl: nextButtonClass,
+          prevEl: prevButtonClass,
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            spaceBetween: 16,
+          },
+          767: {
+            slidesPerView: 4,
+            slidesPerGroup: 1,
+            spaceBetween: 24,
+          },
+        },
+      });
+    }
+  }
+
+  // Инициализация слайдеров
+  if (document.querySelector("#hero")) {
+    new Swiper("#hero", {
+      observer: true,
+      observeParents: true,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".hero-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".hero-button-next",
+        prevEl: ".hero-button-prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 8,
+        },
+      },
+    });
+  }
+
+  // Инициализация слайдеров для других секций
+  initSwiper("#products", ".products-pagination", ".products-button-next", ".products-button-prev");
+});
+
+// swiper
